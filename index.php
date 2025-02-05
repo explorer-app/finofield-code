5601f04466dbc83194ae3bfb53af1c14d93d7f34
Arjun Tripathi <mearjuntripathi@gmail.com>
2025-02-05 16:08:46 +0530

    update value

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
-    <!-- <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico"> -->
-    <link rel="icon" href="./assets/img/logo/loder.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
 
     <!-- CSS here -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
@@ -26,77 +25,12 @@
     <link rel="stylesheet" href="assets/css/slick.css">
     <link rel="stylesheet" href="assets/css/nice-select.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- <link rel="stylesheet" href="/Doc/css/main.css"> -->
 </head>
 
 <body>
     <!-- ? Preloader Start -->
-    <div id="preloader-active">
-        <div class="preloader d-flex align-items-center justify-content-center">
-            <div class="preloader-inner position-relative">
-                <div class="preloader-circle"></div>
-                <div class="preloader-img pere-text">
-                    <img src="assets/img/logo/loder.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Preloader Start-->
-    <header>
-        <!-- Header Start -->
-        <div class="header-area header_area">
-            <div class="main-header">
-                <div class="header-bottom  header-sticky">
-                    <div class="container-fluid">
-                        <div class="row align-items-center">
-                            <!-- Logo -->
-                            <div class="col-xl-2 col-lg-2 col-md-1">
-                                <div class="logo">
-                                    <a href="index.html"><img src="assets/img/logo/logo.png" class="logo-img" alt="">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-xl-10 col-lg-10 col-md-8">
-                                <div class="header-left  d-flex f-right align-items-center">
-                                    <!-- Main-menu -->
-                                    <div class="main-menu f-right d-none d-lg-block">
-                                        <nav>
-                                            <ul id="navigation">
-                                                <li><a href="index.html">Home</a></li>
-                                                <li><a href="#">Pages</a>
-                                                    <ul class="submenu">
-                                                        <li><a href="about.html">about</a></li>
-                                                        <li><a href="elements.html">Element</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="services.html">Services</a></li>
-                                                <li><a href="blog.html">Blog</a>
-                                                    <ul class="submenu">
-                                                        <li><a href="blog.html">Blog</a></li>
-                                                        <li><a href="blog_details.html">Blog Details</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="contact.html">Contact</a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                                    <!-- left Btn -->
-                                    <div class="header-right-btn f-right d-none d-lg-block  ml-30">
-                                        <a href="#" class="header-btn">Make an Appointment</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Mobile Menu -->
-                            <div class="col-12">
-                                <div class="mobile_menu d-block d-lg-none"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header End -->
-    </header>
+    <?php include('./components/header.php')?>
+
     <main>
         <!--? slider Area Start-->
         <div class="slider-area  slider-height position-relative" data-background="assets/img/hero/h1_hero.png">
@@ -593,100 +527,8 @@
         </section>
         <!-- Want To work End -->
     </main>
-    <footer>
-        <div class="footer-wrapper section-bg2 pl-100" data-background="assets/img/gallery/footer_bg.png">
-            <!-- Footer Start-->
-            <div class="footer-area footer-padding">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
-                            <div class="single-footer-caption mb-50">
-                                <div class="single-footer-caption mb-30">
-                                    <!-- logo -->
-                                    <div class="footer-logo mb-35">
-                                        <a href="index.html"><img class="logo-img" src="assets/img/logo/logo2_footer.png" style="width: 300px;" alt=""></a>
-                                    </div>
-                                    <div class="footer-tittle">
-                                        <div class="footer-pera">
-                                            <p>The automated process starts as soon as your clothes go into the machine.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <!-- social -->
-                                    <div class="footer-social">
-                                        <a href="#"><i class="fab fa-twitter"></i></a>
-                                        <a href="https://bit.ly/sai4ull"><i class="fab fa-facebook-f"></i></a>
-                                        <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-6">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Our solutions</h4>
-                                    <ul>
-                                        <li><a href="#">Design & creatives</a></li>
-                                        <li><a href="#">Telecommunication</a></li>
-                                        <li><a href="#">Restaurant</a></li>
-                                        <li><a href="#">Programing</a></li>
-                                        <li><a href="#">Architecture</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-6">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Company</h4>
-                                    <ul>
-                                        <li><a href="#">About</a></li>
-                                        <li><a href="#">Review</a></li>
-                                        <li><a href="#">Insights</a></li>
-                                        <li><a href="#">Programing</a></li>
-                                        <li><a href="#">Carrier</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Contact us</h4>
-                                    <ul>
-                                        <li><a href="#">consulto98@gmail.com</a></li>
-                                        <li><a href="#">76/A, Green road, NYC</a></li>
-                                        <li class="number"><a href="#">(80) 783 367-3904</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- footer-bottom area -->
-            <div class="footer-bottom-area">
-                <div class="container-fluid">
-                    <div class="footer-border">
-                        <div class="row align-items-center">
-                            <div class="col-xl-12 ">
-                                <div class="footer-copy-right">
-                                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                        Copyright &copy;
-                                        <script>document.write(new Date().getFullYear());</script> All rights reserved |
-                                        This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a
-                                            href="https://colorlib.com" target="_blank">Colorlib</a>
-                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Footer End-->
-        </div>
-    </footer>
+    <?php include('./components/footer.php')?>
+
     <!-- Scroll Up -->
     <div id="back-top">
         <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
---
+++ services.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Consulting | Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/progressbar_barfiller.css">
+    <link rel="stylesheet" href="assets/css/gijgo.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/animated-headline.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!-- ? Preloader Start -->
+    <?php include('./components/header.php')?>
+
+    <main>
+        <!--? slider Area Start-->
+        <div class="slider-area slider-area2 slider-height2 position-relative"
+            data-background="assets/img/hero/hero2.png">
+            <div class="slider-active">
+                <!-- Single Slider -->
+                <div class="single-slider">
+                    <div class="slider-cap-wrapper">
+                        <div class="hero-caption">
+                            <h1 data-animation="fadeInLeft" data-delay=".4s">Services</h1>
+                            <!-- breadcrumb Start-->
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="#">Services</a></li>
+                                </ol>
+                            </nav>
+                        </div>
+
+                        <!-- breadcrumb End -->
+                        <div class="hero-img">
+                            <img src="assets/img/hero/h1_hero2.png" alt="" data-animation="fadeInRight"
+                                data-transition-duration="5s">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--? Services Area Start -->
+        <section class="services-section section-padding30 fix">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-10 col-lg-10 col-md-12">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle text-center mb-70">
+                            <span>Our Service</span>
+                            <h2>We bring ideas to life by combining years of experiences of our very talented team.</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-services single-services2 mb-30 section-over1 text-center">
+                            <div class="services-img">
+                                <img src="assets/img/gallery/team1.png" alt="">
+                                <div class="services-caption">
+                                    <h3><a href="#">Business consulting</a></h3>
+                                    <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                    <a href="#" class="btn btn3">Make Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-services single-services2 mb-30 section-over1 text-center">
+                            <div class="services-img">
+                                <img src="assets/img/gallery/team2.png" alt="">
+                                <div class="services-caption">
+                                    <h3><a href="#">Media planing</a></h3>
+                                    <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                    <a href="#" class="btn btn3">Make Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-services single-services2 mb-30 section-over1 text-center">
+                            <div class="services-img">
+                                <img src="assets/img/gallery/team3.png" alt="">
+                                <div class="services-caption">
+                                    <h3><a href="#">Professional HR</a></h3>
+                                    <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                    <a href="#" class="btn btn3">Make Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-services single-services2 mb-30 section-over1 text-center">
+                            <div class="services-img">
+                                <img src="assets/img/gallery/team2.png" alt="">
+                                <div class="services-caption">
+                                    <h3><a href="#">Business consulting</a></h3>
+                                    <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                    <a href="#" class="btn btn3">Make Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-services single-services2 mb-30 section-over1 text-center">
+                            <div class="services-img">
+                                <img src="assets/img/gallery/team1.png" alt="">
+                                <div class="services-caption">
+                                    <h3><a href="#">Media planing</a></h3>
+                                    <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                    <a href="#" class="btn btn3">Make Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-services single-services2 mb-30 section-over1 text-center">
+                            <div class="services-img">
+                                <img src="assets/img/gallery/team3.png" alt="">
+                                <div class="services-caption">
+                                    <h3><a href="#">Professional HR</a></h3>
+                                    <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                    <a href="#" class="btn btn3">Make Appointment</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--Services Area End -->
+        <!--? Want To work -->
+        <section class="wantToWork-area section-bg2" data-background="assets/img/gallery/section_bg01.png">
+            <div class="container">
+                <div class="wants-wrapper w-padding2">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-xl-7 col-lg-7 col-md-12">
+                            <div class="wantToWork-caption wantToWork-caption2">
+                                <h2>Need a consulting services?</h2>
+                                <p>The automated process starts as soon as your clothes go into the machine. Duis
+                                    cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-6">
+                            <a href="#" class="btn w-btn wantToWork-btn mr-20">Make an Appointment</a>
+                            <a href="#" class="btn2 w-btn wantToWork-btn"><i class="fas fa-phone"></i> (89) 673
+                                378-309</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Want To work End -->
+    </main>
+    <?php include('./components/footer.php')?>
+
+    <!-- Scroll Up -->
+    <div id="back-top">
+        <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+    </div>
+
+    <!-- JS here -->
+
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Date Picker -->
+    <script src="./assets/js/gijgo.min.js"></script>
+    <!-- Nice-select, sticky -->
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+    <!-- Progress -->
+    <script src="./assets/js/jquery.barfiller.js"></script>
+
+    <!-- counter , waypoint,Hover Direction -->
+    <script src="./assets/js/jquery.counterup.min.js"></script>
+    <script src="./assets/js/waypoints.min.js"></script>
+    <script src="./assets/js/jquery.countdown.min.js"></script>
+    <script src="./assets/js/hover-direction-snake.min.js"></script>
+
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+
+</body>
+
+</html>
