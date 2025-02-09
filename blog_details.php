34a180425449accabcff27123164759687e10403
Arjun Tripathi <mearjuntripathi@gmail.com>
2025-02-09 22:08:45 +0530

    update

--- blog_details.php
+++ blog_details.php
@@ -73,8 +73,8 @@
                   all gathered us saying he our
                 </h2>
                 <ul class="blog-info-link mt-3 mb-4">
-                  <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
-                  <li><a href="#"><i class="fa fa-comments"></i> 03 Comments</a></li>
+                  <li><i class="fa fa-user"></i> Binod Gupta</li>
+                  <li><i class="fa fa-eye"></i> 03 Views</li>
                 </ul>
                 <p class="excert">
                   MCSE boot camps have its supporters and its detractors. Some people do not understand why you
@@ -108,75 +108,9 @@
                 </p>
               </div>
             </div>
-            <div class="navigation-top">
-              <div class="d-sm-flex justify-content-between text-center">
-                <p class="like-info"><span class="align-middle"><i class="fa fa-heart"></i></span> Lily and 4
-                  people like this</p>
-                <div class="col-sm-4 text-center my-2 my-sm-0">
-                </div>
-                <ul class="social-icons">
-                  <li><a href="https://www.facebook.com/sai4ull"><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                  <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                  <li><a href="#"><i class="fab fa-behance"></i></a></li>
-                </ul>
-              </div>
-              <div class="navigation-area">
-                <div class="row">
-                  <div
-                    class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
-                    <div class="thumb">
-                      <a href="#">
-                        <img class="img-fluid" src="assets/img/post/preview.png" alt="">
-                      </a>
-                    </div>
-                    <div class="arrow">
-                      <a href="#">
-                        <span class="lnr text-white ti-arrow-left"></span>
-                      </a>
-                    </div>
-                    <div class="detials">
-                      <p>Prev Post</p>
-                      <a href="#">
-                        <h4 style="color: #2d2d2d;">Space The Final Frontier</h4>
-                      </a>
-                    </div>
-                  </div>
-                  <div
-                    class="col-lg-6 col-md-6 col-12 nav-right flex-row d-flex justify-content-end align-items-center">
-                    <div class="detials">
-                      <p>Next Post</p>
-                      <a href="#">
-                        <h4 style="color: #2d2d2d;">Telescopes 101</h4>
-                      </a>
-                    </div>
-                    <div class="arrow">
-                      <a href="#">
-                        <span class="lnr text-white ti-arrow-right"></span>
-                      </a>
-                    </div>
-                    <div class="thumb">
-                      <a href="#">
-                        <img class="img-fluid" src="assets/img/post/next.png" alt="">
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="blog-author">
-              <div class="media align-items-center">
-                <img src="assets/img/blog/author.png" alt="">
-                <div class="media-body">
-                  <a href="#">
-                    <h4>Harvard milan</h4>
-                  </a>
-                  <p>Second divided from form fish beast made. Every of seas all gathered use saying you're, he
-                    our dominion twon Second divided from</p>
-                </div>
-              </div>
-            </div>
-            <div class="comments-area">
+            
+            
+            <!-- <div class="comments-area">
               <h4>05 Comments</h4>
               <div class="comment-list">
                 <div class="single-comment justify-content-between d-flex">
@@ -256,38 +190,8 @@
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="comment-form">
-              <h4>Leave a Reply</h4>
-              <form class="form-contact comment_form" action="#" id="commentForm">
-                <div class="row">
-                  <div class="col-12">
-                    <div class="form-group">
-                      <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9"
-                        placeholder="Write Comment"></textarea>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <div class="form-group">
-                      <input class="form-control" name="name" id="name" type="text" placeholder="Name">
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <div class="form-group">
-                      <input class="form-control" name="email" id="email" type="email" placeholder="Email">
-                    </div>
-                  </div>
-                  <div class="col-12">
-                    <div class="form-group">
-                      <input class="form-control" name="website" id="website" type="text" placeholder="Website">
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <button type="submit" class="button button-contactForm btn_1 boxed-btn">Post Comment</button>
-                </div>
-              </form>
-            </div>
+            </div> -->
+            
           </div>
           <div class="col-lg-4">
             <div class="blog_right_sidebar">
@@ -306,47 +210,7 @@
                     type="submit">Search</button>
                 </form>
               </aside>
-              <aside class="single_sidebar_widget post_category_widget">
-                <h4 class="widget_title" style="color: #2d2d2d;">Category</h4>
-                <ul class="list cat-list">
-                  <li>
-                    <a href="#" class="d-flex">
-                      <p>Resaurant food</p>
-                      <p>(37)</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="d-flex">
-                      <p>Travel news</p>
-                      <p>(10)</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="d-flex">
-                      <p>Modern technology</p>
-                      <p>(03)</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="d-flex">
-                      <p>Product</p>
-                      <p>(11)</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="d-flex">
-                      <p>Inspiration</p>
-                      <p>(21)</p>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="d-flex">
-                      <p>Health Care</p>
-                      <p>(21)</p>
-                    </a>
-                  </li>
-                </ul>
-              </aside>
+
               <aside class="single_sidebar_widget popular_post_widget">
                 <h3 class="widget_title" style="color: #2d2d2d;">Recent Post</h3>
                 <div class="media post_item">
@@ -386,81 +250,7 @@
                   </div>
                 </div>
               </aside>
-              <aside class="single_sidebar_widget tag_cloud_widget">
-                <h4 class="widget_title" style="color: #2d2d2d;">Tag Clouds</h4>
-                <ul class="list">
-                  <li>
-                    <a href="#">project</a>
-                  </li>
-                  <li>
-                    <a href="#">love</a>
-                  </li>
-                  <li>
-                    <a href="#">technology</a>
-                  </li>
-                  <li>
-                    <a href="#">travel</a>
-                  </li>
-                  <li>
-                    <a href="#">restaurant</a>
-                  </li>
-                  <li>
-                    <a href="#">life style</a>
-                  </li>
-                  <li>
-                    <a href="#">design</a>
-                  </li>
-                  <li>
-                    <a href="#">illustration</a>
-                  </li>
-                </ul>
-              </aside>
-              <aside class="single_sidebar_widget instagram_feeds">
-                <h4 class="widget_title" style="color: #2d2d2d;">Instagram Feeds</h4>
-                <ul class="instagram_row flex-wrap">
-                  <li>
-                    <a href="#">
-                      <img class="img-fluid" src="assets/img/post/post_5.png" alt="">
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img class="img-fluid" src="assets/img/post/post_6.png" alt="">
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img class="img-fluid" src="assets/img/post/post_7.png" alt="">
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img class="img-fluid" src="assets/img/post/post_8.png" alt="">
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img class="img-fluid" src="assets/img/post/post_9.png" alt="">
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img class="img-fluid" src="assets/img/post/post_10.png" alt="">
-                    </a>
-                  </li>
-                </ul>
-              </aside>
-              <aside class="single_sidebar_widget newsletter_widget">
-                <h4 class="widget_title" style="color: #2d2d2d;">Newsletter</h4>
-                <form action="#">
-                  <div class="form-group">
-                    <input type="email" class="form-control" onfocus="this.placeholder = ''"
-                      onblur="this.placeholder = 'Enter email'" placeholder='Enter email' required>
-                  </div>
-                  <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
-                    type="submit">Subscribe</button>
-                </form>
-              </aside>
+
             </div>
           </div>
         </div>
