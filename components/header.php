69a288635c51fbe756e20a5f6c3915bd23f03277
Arjun Tripathi <mearjuntripathi@gmail.com>
2025-02-10 10:09:04 +0530

    make change like make a blog folder to go blogs where bogs is avilabe

--- components/header.php
+++ components/header.php
@@ -1,65 +1,65 @@
 <div id="preloader-active">
-        <div class="preloader d-flex align-items-center justify-content-center">
-            <div class="preloader-inner position-relative">
-                <div class="preloader-circle"></div>
-                <div class="preloader-img pere-text">
-                    <img src="assets/img/logo/loder.png" alt="">
-                </div>
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-inner position-relative">
+            <div class="preloader-circle"></div>
+            <div class="preloader-img pere-text">
+                <img src="assets/img/logo/loder.png" alt="">
             </div>
         </div>
     </div>
-    <!-- Preloader Start-->
-    <header>
-        <!-- Header Start -->
-        <div class="header-area header_area">
-            <div class="main-header">
-                <div class="header-bottom  header-sticky">
-                    <div class="container-fluid">
-                        <div class="row align-items-center">
-                            <!-- Logo -->
-                            <div class="col-xl-2 col-lg-2 col-md-1">
-                                <div class="logo">
-                                    <a href="./"><img src="assets/img/logo/logo.png" class="logo-img" alt="">
-                                    </a>
+</div>
+<!-- Preloader Start-->
+<header>
+    <!-- Header Start -->
+    <div class="header-area header_area">
+        <div class="main-header">
+            <div class="header-bottom  header-sticky">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <!-- Logo -->
+                        <div class="col-xl-2 col-lg-2 col-md-1">
+                            <div class="logo">
+                                <a href="./"><img src="assets/img/logo/logo.png" class="logo-img" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-10 col-lg-10 col-md-8">
+                            <div class="header-left  d-flex f-right align-items-center">
+                                <!-- Main-menu -->
+                                <div class="main-menu f-right d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="index.php">Home</a></li>
+                                            <li><a href="about.php">About</a></li>
+                                            <li><a href="services.php">Services</a>
+                                                <ul class="submenu">
+                                                    <li><a href="service.php">1. Legal entity</a></li>
+                                                    <li><a href="service.php">2. Tax & Regulatory Advisory</a></li>
+                                                    <li><a href="service.php">3. Accounting / book-keeping</a></li>
+                                                    <li><a href="service.php">4. IFRS conversion </a></li>
+                                                    <li><a href="service.php">5. Representation </a></li>
+                                                    <li><a href="service.php">6. Others </a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="blog.php">Blog</a></li>
+                                            <li><a href="contact.php">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                                <!-- left Btn -->
+                                <div class="header-right-btn f-right d-none d-lg-block  ml-30">
+                                    <a href="#" class="header-btn">Make an Appointment <i class="fa fa-phone"></i></a>
                                 </div>
                             </div>
-                            <div class="col-xl-10 col-lg-10 col-md-8">
-                                <div class="header-left  d-flex f-right align-items-center">
-                                    <!-- Main-menu -->
-                                    <div class="main-menu f-right d-none d-lg-block">
-                                        <nav>
-                                            <ul id="navigation">
-                                                <li><a href="index.php">Home</a></li>
-                                                <li><a href="about.php">About</a></li>
-                                                <li><a href="services.php">Services</a>
-                                                    <ul class="submenu">
-                                                        <li><a href="service.php">1. Legal entity</a></li>
-                                                        <li><a href="service.php">2. Tax & Regulatory Advisory</a></li>
-                                                        <li><a href="service.php">3. Accounting / book-keeping</a></li>
-                                                        <li><a href="service.php">4. IFRS conversion </a></li>
-                                                        <li><a href="service.php">5. Representation </a></li>
-                                                        <li><a href="service.php">6. Others </a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="blog.php">Blog</a></li>
-                                                <li><a href="contact.php">Contact</a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                                    <!-- left Btn -->
-                                    <div class="header-right-btn f-right d-none d-lg-block  ml-30">
-                                        <a href="#" class="header-btn">Make an Appointment <i class="fa fa-phone"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Mobile Menu -->
-                            <div class="col-12">
-                                <div class="mobile_menu d-block d-lg-none"></div>
-                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Header End -->
-    </header>
+    </div>
+    <!-- Header End -->
+</header>
