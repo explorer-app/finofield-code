4c4e96bc5c19dc7440cb647d8e5a297a2d86ec38
mearjuntripathi <rajr6066@gmail.com>
2025-02-21 11:22:00 +0530

    update

--- blog.php
+++ blog.php
@@ -178,7 +178,7 @@
                 <form action="./blogs.php" method="get">
                   <div class="form-group">
                     <div class="input-group mb-3">
-                      <input type="text" class="form-control" placeholder='Search Keyword'
+                      <input type="text" name="query" value="<?=$query?>" class="form-control" placeholder='Search Keyword'
                         onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'">
                       <div class="input-group-append">
                         <button class="btns" type="submit"><i class="ti-search"></i></button>
